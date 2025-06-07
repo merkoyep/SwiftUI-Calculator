049339a76198be8f0eb4807e1c558aef1a78750c
merkoyep <81783308+merkoyep@users.noreply.github.com>
2025-06-06 23:59:00 -0400

    Initial Commit

---
+++ Calculator-SwiftUi/Calculator_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_SwiftUiApp.swift
+//  Calculator-SwiftUi
+//
+//  Created by Marco on 2025-06-06.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
